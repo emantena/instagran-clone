ed88a48191e2d53689a5e417c4159a9fff6fa4d5
everton mantena <evertonmantena@gmail.com>
2018-09-14 15:06:35 -0300

    criando componentes Acesso, Banner, Login e Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { AcessoComponent } from './acesso/acesso.component';
+import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, AcessoComponent, BannerComponent, LoginComponent, CadastroComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
